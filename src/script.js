b02fb83409a1eab4434eec336f0b17f74a67fee1
Tri <trimpham98@gmail.com>
2023-05-26 12:51:51 -0700

    Add OrbitControls using mouse

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {OrbitControls} from '/Users/trimpham98/repos/threejs-journey-07-cameras/node_modules/three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Cursor
@@ -61,6 +62,9 @@ camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -77,10 +81,10 @@ const tick = () => {
   //   mesh.rotation.y = elapsedTime;
 
   // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+//   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+//   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+//   camera.position.y = cursor.y * 5;
+//   camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
