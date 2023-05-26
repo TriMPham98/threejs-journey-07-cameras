6106fecb29712cb039406efced5824e1cb30b813
Tri <trimpham98@gmail.com>
2023-05-26 10:58:38 -0700

    Add OrthographicCamera with aspectRatio for left/right parameters

--- src/script.js
+++ src/script.js
@@ -23,7 +23,18 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+
+const aspectRatio = sizes.width / sizes.height;
+console.log(aspectRatio);
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+);
 camera.position.x = 2;
 camera.position.y = 2;
 camera.position.z = 2;
