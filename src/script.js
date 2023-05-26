bf0b2b9eff0c13ba377d1c5afafd6d0e2cb7fdc8
Tri <trimpham98@gmail.com>
2023-05-26 12:54:51 -0700

    Add damping for smooth deceleration of camera position

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import {OrbitControls} from '/Users/trimpham98/repos/threejs-journey-07-cameras/node_modules/three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from "/Users/trimpham98/repos/threejs-journey-07-cameras/node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 /**
  * Cursor
@@ -64,6 +64,7 @@ scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -81,10 +82,13 @@ const tick = () => {
   //   mesh.rotation.y = elapsedTime;
 
   // Update camera
-//   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-//   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-//   camera.position.y = cursor.y * 5;
-//   camera.lookAt(mesh.position);
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   camera.lookAt(mesh.position);
+
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
